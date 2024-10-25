d36447459ea2e20abe60c2d6e9c12bff0e3eea5b
TH <ok821000@gmail.com>
2024-10-26 00:12:32 +0900

    [Add] SpringBoot Study - ~Section4 진행 - Service단 생성 후 테스트 케이스 생성 및 확인

---
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,58 @@
+package hello.hello_spring.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+/* Ctrl Shift T로 테스트 케이스를 생성*/
+public class MemberService {
+
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /* TestCase에서 memberRepository를 받아옴 (D.I 의존성주입) */
+    private final MemberRepository memberRepository;
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /*
+    public Long join(Member member) {
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m ->{
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+
+        memberRepository.save(member);
+        return member.getId();
+    Optional을 바로 반환하는 게 좋지 않은 이유?
+    */
+
+    public Long join(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m ->{
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+        
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /* Ctrl Alt m으로 method를 생성 */
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
